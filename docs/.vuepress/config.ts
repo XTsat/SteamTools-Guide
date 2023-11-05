6808aa4cfd88bc61f0677a838efc49e005f74b6c
XTsat <44708609+XTsat@users.noreply.github.com>
2023-11-05 10:43:59 +0800

    locales

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -51,18 +51,18 @@ export default defineUserConfig({
     // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ],
 
-  locales: {
-    '/zh/': {
-      lang: 'zh_CN',
-      title: 'Watt Toolkit 指南',
-      description: '「Watt Toolkit」是一个开源跨平台的多功能 Steam 工具箱。',
-    },
-    '/en/': {
-      lang: 'en_US',
-      title: 'Watt Toolkit Guide',
-      description: 'Watt Toolkit" is an open source cross-platform multi-purpose game toolkit',
-    },
-  },
+  // locales: {
+  //   '/zh/': {
+  //     lang: 'zh_CN',
+  //     title: 'Watt Toolkit 指南',
+  //     description: '「Watt Toolkit」是一个开源跨平台的多功能 Steam 工具箱。',
+  //   },
+  //   '/en/': {
+  //     lang: 'en_US',
+  //     title: 'Watt Toolkit Guide',
+  //     description: 'Watt Toolkit" is an open source cross-platform multi-purpose game toolkit',
+  //   },
+  // },
 
   theme,
 
