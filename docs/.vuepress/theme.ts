d4e160e3e3ffed92e91fd0728c3ec23899d51c73
XTsat <44708609+XTsat@users.noreply.github.com>
2023-11-05 19:40:26 +0800

    plugins add

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -65,74 +65,24 @@ export default hopeTheme({
       reaction: false,
     },
 
-    // components: {
-    //   components: ["Badge", "BiliBili"],
-    // },
+    components: {
+      components: ["Badge", "BiliBili"],
+    },
 
-    // feed: {
-    //   rss: true,
-    // },
+    feed: {
+      rss: true,
+    },
 
-    // sitemap: {
-    //   changefreq: "weekly",
-    // },
+    sitemap: {
+      changefreq: "weekly",
+    },
 
-    // mdEnhance: {
-    //   gfm: true,
-    //   imgLazyload: true,
-    //   include: true,
-    //   tabs: true,
-    // },
+    mdEnhance: {
+      gfm: true,
+      imgLazyload: true,
+      include: true,
+      tabs: true,
+    },
 
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: false,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   themeColor: "#f26d6d",
-    //   update: "hint",
-    //   apple: {
-    //     icon: "/favicon.ico",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/favicon.ico",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/pwa-icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/pwa-icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/pwa-icon/chrome-144.png",
-    //         sizes: "144x144",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/pwa-icon/chrome-72.png",
-    //         sizes: "72x72",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/pwa-icon/chrome-96.png",
-    //         sizes: "96x96",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/pwa-icon/chrome-48.png",
-    //         sizes: "48x48",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //   },
-    // },
   },
 });
